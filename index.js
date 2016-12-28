3dc346147f58d64e687173e1a4d7553e62c9f82a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-12-28 22:17:09 +0100

    Error response

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ request.get = function (url, callback) {
 
     callback(null, response, xhr.responseText)
   })
+
+  xhr.addEventListener('error', function () {
+    var response = {
+      statusCode: xhr.status
+    }
+
+    callback(xhr.responseText, response, null)
+  })
 }
 
 module.exports = request
